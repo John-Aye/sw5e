4f565e8b94fd07fc58ce2cd0040488b1559c19eb
John-Aye <eragon1011@hotmail.com>
2020-05-20 13:02:21 -0500

    added all accessories

---
+++ app/src/main/java/com/example/sw5ecompanion/models/equipment/accessories/FrictionGripGear.java
@@ -0,0 +1,16 @@
+package com.example.sw5ecompanion.models.equipment.accessories;
+
+public class FrictionGripGear extends Accessories {
+
+    public FrictionGripGear() {
+
+        super();
+        this.setName("Friction-Grip Gear");
+        this.setCost(2000);
+        this.setWeight(12);
+        this.setDescription("A special set of gloves and boots that allow the wearer to stick to " +
+                "and climb surfaces.  While wearing these items, you gain the ability to move " +
+                "along vertical surfaces and ceilings while leaving your hands free.  You also " +
+                "gain a climbing speed equal you walking speed.");
+    }
+}
---
+++ app/src/main/java/com/example/sw5ecompanion/models/equipment/accessories/TrackerUtilityVest.java
@@ -0,0 +1,17 @@
+package com.example.sw5ecompanion.models.equipment.accessories;
+
+public class TrackerUtilityVest extends Accessories {
+
+    public TrackerUtilityVest() {
+
+        super();
+        this.setName("Tracker Utility Vest");
+        this.setCost(150);
+        this.setWeight(3);
+        this.setDescription("A utility vest is a sleeveless item of clothing that includes several " +
+                "evenly-distributed pockets, popular among hunters, smugglers, and adventurers.  " +
+                "The vest is made of leathery, hide material to resist rips, corrosion, and water." +
+                "  The wearer can carry up to 10 light items (up to 1 pound each) without " +
+                "increasing their total encumbrance.");
+    }
+}
